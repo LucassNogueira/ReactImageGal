62c9cb86bcb30a1d5bc9314c9beaa97f56dc8f60
LucassNogueira <simplylucasjason@gmail.com>
2022-03-18 15:28:31 -0500

    fixed style

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,12 @@ import React from "react";
 const title = () => {
   return (
     <div className="title">
-      <h1>Image Gall</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Beautiful Photos</h1>
+      <h2>What makes you happy?</h2>
+      <p>
+        Please post a photo of something that makes you happy, images are hosted
+        so please keep it clean!
+      </p>
     </div>
   );
 };
